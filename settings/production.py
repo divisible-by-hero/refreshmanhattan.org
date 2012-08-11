8c7205dedcf4ae7cfcf18f1067fb19ac4b3fa775
Derek Stegelman <dstegelman@gmail.com>
2012-08-11 16:42:34 -0500

    Deployment solution.

--- settings/production.py
+++ settings/production.py
@@ -1,5 +1,5 @@
 from settings.common import *
-
+from refresh.settings.production import PROD_DB_NAME, PROD_DB_PASSWORD
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -10,9 +10,9 @@ THUMBNAIL_DEBUG = DEBUG
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'NAME': PROD_DB_NAME,                      # Or path to database file if using sqlite3.
+        'USER': PROD_DB_NAME,                      # Not used with sqlite3.
+        'PASSWORD': PROD_DB_PASSWORD,                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -38,26 +38,4 @@ STATIC_ROOT = ''
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = ''
 
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    "",
-    
-    # We have added a "Master" template directory here, and below if you choose, add directories for each specific app.
-    #"./newapp/templates"
-)
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-
-
-)
 
